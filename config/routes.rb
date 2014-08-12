d4778ac7e4eca8e98000bf24cd16497df936b537
OlegSilakov <enegizer93@gmail.com>
2014-08-12 09:54:18 +0000

    add person controller for devise autorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Pastebin::Application.routes.draw do
+  get "persons/profile", as: "user_root"
   get "users/index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
