82b459dc399a75a93346a123cf46ec6718245498
OlegSilakov <enegizer93@gmail.com>
2014-08-12 07:37:07 +0000

    init rails application with cancan autorization gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pastebin::Application.config.secret_key_base = '3d0b3bd04be4be59d8a3d791e6dce3485031efdf7247802e9a8d36c7a6f6bdf4dbfb203ac44d4e3ff078370bfcf18d6c9517a9d7663ef1fd261092967a128eb8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pastebin::Application.config.session_store :cookie_store, key: '_pastebin_session'
