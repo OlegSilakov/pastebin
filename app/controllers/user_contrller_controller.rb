5ab47e0caeebf8b662aac565d83a2f8f075f9bd8
OlegSilakov <o.silakov@fun-box.ru>
2014-08-14 09:56:52 +0000

    delete user_contrller

--- app/controllers/user_contrller_controller.rb
+++
@@ -1,8 +0,0 @@
-class UserContrllerController < ApplicationController
-	load_and_authorize_resource
-
-	def index
-	  authorize! :index, Project
-    @projects = Project.accessible_by(current_ability)	
-	end
-end
