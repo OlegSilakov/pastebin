5b362056dc5794a2a56ad4ac330f0245b5ea0244
OlegSilakov <o.silakov@fun-box.ru>
2014-08-15 12:48:08 +0000

    change user registration tests

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -1,22 +1,17 @@
 require "rails_helper"
 
 describe "user registration" do
+  
   it "allows new users to register with an email address and password" do
-    user = User.create(:email    => "alindeman@example.com",
-                       :password => "ilovegrapes",
-                       :password_confirmation => "ilovegrapes")
-
-    DatabaseCleaner.clean
-    # user = FactoryGirl.create(:user)
 
     visit "/users/sign_up"
 
-    fill_in "Email",                 :with => user.email
-    fill_in "Password",              :with => user.password
-    fill_in "Password confirmation", :with => user.password_confirmation
+    fill_in "Email",                 :with => "alindeman@example.com"
+    fill_in "Password",              :with => "ilovegrapes"
+    fill_in "Password confirmation", :with => "ilovegrapes"
 
     click_button "Sign up"
-    
+
     expect(page).to have_content("Welcome! You have signed up successfully.")
   end
 end
