95cd4e04f78f534af50774b8889f19bb8b6249c2
OlegSilakov <o.silakov@fun-box.ru>
2014-08-19 10:06:59 +0000

    Refactor code after rebase

--- app/models/logpost.rb
+++ app/models/logpost.rb
@@ -1,3 +1,4 @@
 class Logpost < ActiveRecord::Base
+  validates :user_mail, :content, :date, presence: true
   belongs_to :user
 end
--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -8,10 +8,12 @@ FactoryGirl.define do
     password "ilovegrapes"
     password_confirmation "ilovegrapes"
   end
+end
 
+FactoryGirl.define do
   factory :logpost do
-    email
+    user_mail "ex3@mail.com"
     content "[13263] 2014-03-03 15:11:45 Request: /app_tracker/v1/callback"
-    date 2014-08-14 12:53:01
+    date "2014-08-14 12:53:01"
   end
 end
