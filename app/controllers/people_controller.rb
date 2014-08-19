1b7988bd8ea07c623ec51f4c568f89177db41b0b
OlegSilakov <o.silakov@fun-box.ru>
2014-08-19 10:36:24 +0000

    Refactor name

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,6 @@
 class PeopleController < ApplicationController
   before_action :authenticate_user!
   def profile
-  	@logpost = Logpost.where(user_mail: current_user.email)
+  	@current_user_logposts = Logpost.where(user_mail: current_user.email)
   end
 end
