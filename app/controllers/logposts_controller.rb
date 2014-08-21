21fdf118f82b0a6a77ebfbd1b758affcda85e973
o.silakov <o.silakov@fun-box.ru>
2014-08-21 14:03:34 +0000

    refactor to view

--- app/controllers/logposts_controller.rb
+++ app/controllers/logposts_controller.rb
@@ -2,7 +2,6 @@ class LogpostsController < ApplicationController
   before_action :authenticate_user!, :set_logpost, only: [:show, :edit, :update, :destroy]
 
   def show
-    @current_url = logpost_url(@logpost)
   end
 
   def new
