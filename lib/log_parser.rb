698eb24ae377c0b127c74909420a15f366aa07ef
o.silakov <o.silakov@fun-box.ru>
2014-08-22 11:08:26 +0000

    Add log syntax highlight

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,13 +1,37 @@
 module Parser
   def self.parse_log(text)
-    puts text
-    text.split(' ').reduce([]) do |acc, val|
-      if val.include? '['
-        # puts val
-        acc << "<mark>#{val}</mark>"
-      else
-        acc << val
-      end  
-    end.join
+    text = parse_date(text)
+    
+    ['[', ']', '{', '}'].each do |v|
+      text["#{v}"] = "<font color=\"red\">#{v}</font>" if text.include? v
+    end    
+    text
+  end
+
+  # def self.parse_numbers(text)
+  #   text.split(/\D+/).each() do |val|
+  #     if val.to_i > 0 
+  #       puts val
+  #       text["#{val}"] = "<font color=\"blue\">#{val}</font>"
+  #     end
+  #   end
+  #   text
+  # end
+
+  def self.parse_date(text)
+    text.match(/\d{4}\-\d{1,2}\-\d{1,2}/).to_a.each() do |val|
+      text["#{val}"] = "<font color=\"blue\">#{val}</font>"
+    end
+    text
+  end
+
+  def self.parse_time(text)
+    text.match(/\d{2}\:\d{2}\:\d{2}/).to_a.each() do |val|
+      text["#{val}"] = "<font color=\"navy\">#{val}</font>"
+    end
+  end
+
+  def self.parse_telephone(text)
+    
   end
 end
