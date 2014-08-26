c7f12ca55100c657cbdba01a00208b941c273703
o.silakov <o.silakov@fun-box.ru>
2014-08-26 08:05:05 +0000

    Refactor

--- lib/decorator.rb
+++ lib/decorator.rb
@@ -1,6 +1,5 @@
 module Decorator
   def self.decorate_post(text)
-    puts text
     text.split(%r{\n}).reduce([]) do |acc, v|
       acc << "<a href = '#L#{acc.length}' style='text-decoration: none'><code class='language-javascript'>#{v}</code></a>"
     end.join
